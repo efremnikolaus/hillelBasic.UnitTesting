1d48c0204187653acb01e9f34cfb3c83935a6dd6
Nikolai <efremovnikolaus@gmail.com>
2022-12-07 18:07:02 +0100

    precision

--- src/main/java/Arithmetic/Main.java
+++ src/main/java/Arithmetic/Main.java
@@ -3,7 +3,7 @@ package Arithmetic;
 public class Main {
     public static void main(String[] args) {
 
-        double[] array = {3,7,10,18};
+        double[] array = {3, 7, 10, 18};
         double average = new Array().arithmeticAverage(array);
         System.out.println("array average is: " + average);
     }
